0272b6d4ee438da6762346357858356dcb92290f
faustofuga <faustofugaf@icloud.com>
2025-05-31 20:41:08 -0300

    teste banco

--- server.js
+++ server.js
@@ -1,21 +1,28 @@
+
 require('dotenv').config();
 const express = require('express');
 const session = require('express-session');
 const path = require('path');
-const app = express();
 
-app.use(express.static('public'));
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 app.use(session({
-  secret: 'sistemaerp-secret',
+  secret: 'requestti-secret',
   resave: false,
   saveUninitialized: true
 }));
 
+// View Engine
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+// Routes
 app.use('/', require('./routes/index'));
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
