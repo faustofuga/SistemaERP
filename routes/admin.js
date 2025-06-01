63c6b03cde3f026714866d3767775ce8882c9c0f
faustofuga <faustofugaf@icloud.com>
2025-06-01 00:16:05 -0300

    teste

--- routes/admin.js
+++ routes/admin.js
@@ -9,7 +9,7 @@ router.get('/dashboard', (req, res) => {
 
     res.render('admin/dashboard', { user: req.session.user });
   } catch (error) {
-    console.error('Erro em /admin/dashboard:', error);
+    console.error('Erro ao carregar /admin/dashboard:', error);
     res.status(500).send('Erro interno no servidor');
   }
 });
