05b9777f3fc291195b8752462012c78e0a84f0b8
faustofuga <faustofugaf@icloud.com>
2025-06-01 00:19:04 -0300

    t

--- routes/admin.js
+++ routes/admin.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/dashboard', (req, res) => {
+<<<<<<< HEAD
   try {
     if (!req.session.user || req.session.user.tipo !== 'admin') {
       return res.redirect('/login');
@@ -11,7 +12,12 @@ router.get('/dashboard', (req, res) => {
   } catch (error) {
     console.error('Erro ao carregar /admin/dashboard:', error);
     res.status(500).send('Erro interno no servidor');
+=======
+  if (!req.session.user || req.session.user.tipo !== 'admin') {
+    return res.redirect('/login');
+>>>>>>> parent of 08fc010 (Update admin.js)
   }
+  res.render('admin/dashboard', { user: req.session.user });
 });
 
 module.exports = router;
